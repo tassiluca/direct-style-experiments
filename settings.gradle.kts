ed99b695d41c813b80548a787e1d0d12d2692392
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-12-29 12:32:11 +0100

    chore: add git hook for conventional commits

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.17"
+}
+
+gitHooks {
+    commitMsg {
+        conventionalCommits()
+    }
+    createHooks()
+}
+
 rootProject.name = "PPS-22-direct-style-experiments"
