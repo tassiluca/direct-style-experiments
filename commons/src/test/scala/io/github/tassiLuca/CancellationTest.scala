4aa6a8e24fa641d7e1ed5542719f1482e4966e8f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-02-08 11:59:41 +0100

    test: improve cancellation test

--- commons/src/test/scala/io/github/tassiLuca/CancellationTest.scala
+++ commons/src/test/scala/io/github/tassiLuca/CancellationTest.scala
@@ -3,56 +3,85 @@ package io.github.tassiLuca
 import gears.async.AsyncOperations.sleep
 import gears.async.default.given
 import gears.async.{Async, Future}
-import io.github.tassiLuca.boundaries.EitherConversions.given
-import io.github.tassiLuca.boundaries.either
-import io.github.tassiLuca.boundaries.either.?
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
-import scala.util.{Failure, Try}
+class CancellationTest extends AnyFunSpec with Matchers {
 
-class CancellationTest extends AnyFlatSpec with Matchers {
+  describe("Structured concurrency") {
+    it("ensure all nested computations are contained within the lifetime of the enclosing one") {
+      Async.blocking:
+        val before = System.currentTimeMillis()
+        val f = Future:
+          val f1 = Future { "hello" }
+          val f2 = Future { sleep(2_000); "gears!" }
+          f1.await + " " + f2.await
+        f.await shouldBe "hello gears!"
+        val now = System.currentTimeMillis()
+        now - before should be > 2_000L
+    }
 
-  "Structured concurrency with cancellation" should "be supported" in {
-    Async.blocking:
-      val before = System.currentTimeMillis()
-      val f =
-        either:
-          val f1 = Future { sleep(2_000); "hello" }
-          val f2 = Future { throw IllegalStateException() }
-          f1.awaitResult.? + f2.awaitResult.?
-      println(f)
-      val now = System.currentTimeMillis()
-      now - before should be > 2_000L
-  }
+    describe("in case of failures") {
+      it("if the first nested computation we wait fails with an exception the other is cancelled") {
+        Async.blocking:
+          var stillAlive = false
+          val before = System.currentTimeMillis()
+          val f = Future:
+            val f1 = Future { throw Error(); "hello" }
+            val f2 = Future { sleep(2_000); stillAlive = true }
+            f1.await + " " + f2.await // fortunate case in which the one which fails is the one we wait for
+          f.awaitResult.isFailure shouldBe true
+          val now = System.currentTimeMillis()
+          now - before should be < 2_000L
+          sleep(3_000)
+          stillAlive shouldBe false
+      }
 
-  "Zip" should "**NOT** cancel if the first fails" in {
-    val collector = scala.collection.mutable.Set[String]()
-    Async.blocking:
-      val before = System.currentTimeMillis()
-      val f1 = Future { sleep(2_000); collector.add("hello") }
-      val f2 = Future { throw IllegalStateException(); collector.add("world") }
-      val ftot = f1.zip(f2)
-      ftot.awaitResult.isFailure shouldBe true
-      val now = System.currentTimeMillis()
-      now - before should be < 2_000L
-      collector shouldBe empty
-      sleep(2_000)
-      collector should contain("hello")
-  }
+      it("if a nested computation fails while we are waiting for another, the enclosing future is not cancelled") {
+        Async.blocking:
+          var stillAlive = false
+          val before = System.currentTimeMillis()
+          val f = Future:
+            val f1 = Future { throw Error(); "gears!" }
+            val f2 = Future { sleep(2_000); stillAlive = true; "hello" }
+            f2.await + " " + f1.await
+          f.awaitResult.isFailure shouldBe true
+          val now = System.currentTimeMillis()
+          now - before should be > 2_000L
+          stillAlive shouldBe true
+      }
 
-  "Await all with cancellation" should "cancel if the first fails" in {
-    val collector = scala.collection.mutable.Set[String]()
-    Async.blocking:
-      val before = System.currentTimeMillis()
-      val f1 = Future { sleep(2_000); collector.add("hello") }
-      val f2 = Future { throw IllegalStateException(); collector.add("world") }
-      val ftot = Try { Seq(f1, f2).awaitAllOrCancel }
-      ftot.isFailure shouldBe true
-      val now = System.currentTimeMillis()
-      now - before should be < 2_000L
-      collector shouldBe empty
-      sleep(2_000)
-      collector shouldBe empty
+      it("but we can achieve cancellation using combinators") {
+        Async.blocking:
+          var stillAlive = false
+          val before = System.currentTimeMillis()
+          val f = Future:
+            val f1 = Future { throw Error(); "gears!" }
+            val f2 = Future { sleep(2_000); stillAlive = true; "hello" }
+            f2.zip(f1).await
+          f.awaitResult.isFailure shouldBe true
+          val now = System.currentTimeMillis()
+          now - before should be < 2_000L
+          sleep(3_000)
+          stillAlive shouldBe false
+      }
+    }
+
+    describe("allows racing futures cancelling the slower one when one succeeds") {
+      Async.blocking:
+        var stillAlive = false
+        val before = System.currentTimeMillis()
+        val f = Future:
+          val f1 = Future { sleep(1_000); "faster won" }
+          val f2 = Future { sleep(2_000); stillAlive = true }
+          f1.altWithCancel(f2).await
+        val result = f.awaitResult
+        val now = System.currentTimeMillis()
+        now - before should (be > 1_000L and be < 5_000L)
+        result.isSuccess shouldBe true
+        result.get shouldBe "faster won"
+        sleep(3_000)
+        stillAlive shouldBe false
+    }
   }
 }
