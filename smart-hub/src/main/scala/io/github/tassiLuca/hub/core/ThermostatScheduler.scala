60e572a4ce152f71eb2fb1a6612821a4d98ea688
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-02-23 09:47:55 +0100

    refactor(hub): improve scheduler code readability

--- smart-hub/src/main/scala/io/github/tassiLuca/hub/core/ThermostatScheduler.scala
+++ smart-hub/src/main/scala/io/github/tassiLuca/hub/core/ThermostatScheduler.scala
@@ -12,17 +12,18 @@ trait ThermostatScheduler:
 
   extension (date: LocalDateTime) def in(t: TimeSpan): Boolean
 
-trait ByHourThermostatScheduler extends ThermostatScheduler:
+trait ThermostatHourlyScheduler extends ThermostatScheduler:
   override type TimeSpan = (Int, Int)
   override type Day = DayOfWeek
+  type Schedule = Map[(Day, TimeSpan), Temperature]
 
 object ThermostatScheduler:
 
-  def byHour(targetTemperature: Temperature): ByHourThermostatScheduler = ThermostatSchedulerImpl(targetTemperature)
+  def byHour(targetTemperature: Temperature): ThermostatHourlyScheduler = ThermostatSchedulerImpl(targetTemperature)
 
-  private class ThermostatSchedulerImpl(val targetTemperature: Temperature) extends ByHourThermostatScheduler:
+  private class ThermostatSchedulerImpl(val targetTemperature: Temperature) extends ThermostatHourlyScheduler:
 
-    private var schedule: Map[(Day, TimeSpan), Temperature] = Map()
+    private var schedule: Schedule = Map()
 
     override def targetFor(date: LocalDateTime): Temperature = schedule
       .find((d, _) => date.getDayOfWeek == d._1 && (date in d._2))
@@ -30,22 +31,23 @@ object ThermostatScheduler:
       .getOrElse(targetTemperature)
 
     override def update(day: Day, timeSpan: TimeSpan, target: Temperature): Unit =
-      require(timeSpan._1 < timeSpan._2)
-      if schedule.isEmpty then schedule = schedule + ((day, timeSpan) -> target)
+      require(timeSpan._1 < timeSpan._2, "The timespan end must be greater than the start")
+      val newEntry = (day, timeSpan) -> target
+      if schedule.isEmpty then schedule = schedule + newEntry
       else
-        val (nonOverlapping, overlapping) = findOverlapping(day, timeSpan)
-        val updatedEntries = overlapping.flatMap { case ((day, (start, end)), currentTarget) =>
-          List(
-            (day, (start, timeSpan._1)) -> currentTarget,
-            (day, (timeSpan._2, end)) -> currentTarget,
-          )
-        }.filter { case ((_, (start, end)), _) => start < end }
-        schedule = (nonOverlapping ++ updatedEntries) + ((day, timeSpan) -> target)
+        val (nonOverlapping, overlapping) = overlappingIn(day, timeSpan)
+        val splitEntries = overlapping.splitFor(timeSpan)
+        schedule = (nonOverlapping ++ splitEntries) + newEntry
 
     extension (date: LocalDateTime)
-      override def in(timeSpan: TimeSpan): Boolean = timeSpan._1 <= date.getHour && timeSpan._2 >= date.getHour
+      override infix def in(timeSpan: TimeSpan): Boolean =
+        timeSpan._1 <= date.getHour && timeSpan._2 >= date.getHour
 
-    private def findOverlapping(day: Day, timeSpan: TimeSpan) =
-      schedule.partition { case ((d, (start, end)), _) =>
-        d == day && (end <= timeSpan._1 || start >= timeSpan._2)
-      }
+    private def overlappingIn(day: Day, timeSpan: TimeSpan) =
+      schedule.partition { case ((d, (start, end)), _) => d == day && (end <= timeSpan._1 || start >= timeSpan._2) }
+
+    extension (schedule: Schedule)
+      private def splitFor(timeSpan: TimeSpan) =
+        schedule.flatMap { case ((day, (start, end)), currentTarget) =>
+          (day, (start, timeSpan._1)) -> currentTarget :: (day, (timeSpan._2, end)) -> currentTarget :: Nil
+        }.filter { case ((_, (start, end)), _) => start < end }
