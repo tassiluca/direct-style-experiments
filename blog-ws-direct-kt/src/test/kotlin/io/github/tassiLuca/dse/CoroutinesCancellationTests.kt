94d38aac9b280bfcefbd65d6e45a0af71231d35e
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-03-04 18:11:21 +0100

    test: fix cancellation coroutine supervisor test

--- blog-ws-direct-kt/src/test/kotlin/io/github/tassiLuca/dse/CoroutinesCancellationTests.kt
+++ blog-ws-direct-kt/src/test/kotlin/io/github/tassiLuca/dse/CoroutinesCancellationTests.kt
@@ -13,28 +13,27 @@ class CoroutinesCancellationTests : FunSpec() {
     init {
         test("in coroutineScope, a failure of a nested coroutine propagates upwards, cancelling all other ones") {
             var job2Completed = false
-            runCatching {
+            val result = runCatching {
                 coroutineScope {
                     val job1 = failingJob()
                     val job2 = successfulJob { job2Completed = true }
                     job2.await() // please note we wait first for the successful job
                     job1.await()
-                    job2Completed shouldBe false
                 }
             }
+            result.isFailure shouldBe true
+            job2Completed shouldBe false
         }
 
         test("supervisorScope does not propagate, allowing other coroutines to complete") {
             var job2Completed = false
-            runCatching {
-                supervisorScope {
-                    val job1 = failingJob()
-                    val job2 = successfulJob { job2Completed = true }
-                    job1.await()
-                    job2.await()
-                    job2Completed shouldBe true
-                }
+            supervisorScope {
+                val job1 = failingJob()
+                val job2 = successfulJob { job2Completed = true }
+                runCatching { job1.await() }.isFailure shouldBe true
+                runCatching { job2.await() }.isSuccess shouldBe true
             }
+            job2Completed shouldBe true
         }
     }
 
