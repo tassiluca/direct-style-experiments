db8c3165fd2e1242819349b81827ab730725e736
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-03-09 16:23:19 +0100

    chore: move to gradle catalog deps

--- analyzer-direct-kt/build.gradle.kts
+++ analyzer-direct-kt/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("plugin.serialization") version "1.9.22"
+    alias(libs.plugins.kotlin.serialization)
 }
 
 dependencies {
--- analyzer-monadic/build.gradle.kts
+++ analyzer-monadic/build.gradle.kts
@@ -2,8 +2,8 @@ dependencies {
     implementation(libs.sttp)
     implementation(libs.sttp.upickle)
     implementation(libs.cats.core)
-    implementation("com.softwaremill.sttp.client3:async-http-client-backend-monix_3:3.9.3")
-    implementation("io.monix:monix_3:3.4.1")
+    implementation(libs.sttp.backend.monix)
+    implementation(libs.monix)
     implementation(project(":analyzer-commons"))
 }
 
